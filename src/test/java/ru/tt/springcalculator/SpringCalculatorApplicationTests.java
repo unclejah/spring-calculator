155bf8fdcdaaa0af5d57e29fc3a9a4c295b81b85
jah <celt8@yandex.ru>
2022-09-19 10:45:33 +0500

    - init commit

---
+++ src/test/java/ru/tt/springcalculator/SpringCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tt.springcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
